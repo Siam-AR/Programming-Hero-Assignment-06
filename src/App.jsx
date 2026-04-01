e6b1908ad907bd6199c41cb63a7d985105cf952d
Siam-AR <siam.ar.nexus@gmail.com>
2026-04-01 20:13:57 +0600

    feat(stats): implement stats section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Stats from "./components/Stats/Stats";
 
 function App() {
   return (
     <div className="min-h-screen bg-white text-gray-900">
       <Navbar></Navbar>
       <Hero></Hero>
+      <Stats></Stats>
     </div>
   );
 }
